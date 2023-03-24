c6d159b59e3041c75190bc93b78a91462b32b20e
pain <legendofpain5112@gmail.com>
2023-03-24 22:16:02 +0500

    header + hero section completed, adaptived

---
+++ dist/assets/js/main.js
@@ -0,0 +1,26 @@
+function burgerMenu() {
+	const burger = document.querySelector(".burger")
+	const headerCategories = document.querySelector(".header__categories")
+	const overlay = document.querySelector(".overlay")
+
+	burger.addEventListener("click", () => {
+		burger.classList.toggle("active")
+		headerCategories.classList.toggle("active")
+		overlay.classList.toggle("active")
+	})
+
+	overlay.addEventListener("click", () => {
+		burger.classList.remove("active")
+		headerCategories.classList.remove("active")
+		overlay.classList.remove("active")
+	})
+
+	window.addEventListener("resize", () => {
+		if (window.innerWidth > 991.98) {
+			burger.classList.remove("active")
+			headerCategories.classList.remove("active")
+			overlay.classList.remove("active")
+		}
+	})
+}
+burgerMenu()
